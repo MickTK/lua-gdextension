db99a5579c3f935f85f5e44bcd4720a8f77b9568
Michele <63038410+MickTK@users.noreply.github.com>
2026-01-05 11:29:08 +0100

    Merge fixes

--- test/lua_tests/variant_constants.lua
+++
@@ -1,7 +0,0 @@
--- Constant values
-assert(Vector2.ZERO ~= nil)
-assert(Vector2.ZERO == Vector2(0, 0))
-
--- Enums
-assert(Vector2.Axis ~= nil)
-assert(Vector2.Axis.AXIS_X ~= nil)
--- tools/code_generator.py
+++ tools/code_generator.py
@@ -13,13 +13,11 @@ def generate(env):
             "src/generated/global_enums.hpp",
             "src/generated/utility_functions.hpp",
             "src/generated/package_searcher.h",
-            "src/generated/lua_script_globals.h",
             "src/generated/variant_type_constants.hpp",
         ],
         [
             "tools/code_generation/generate_cpp_code.py",
             "src/luaopen/package_searcher.lua",
-            "src/script-language/globals.lua",
             "lib/godot-cpp/gdextension/extension_api.json",
             "lib/godot-cpp/gen/include/godot_cpp/variant/utility_functions.hpp",
         ],
